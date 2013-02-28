3c048e5202d4be38ff476ce0e72cf395cff14508
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-02-28 16:00:52 +0100

    Reading a bunch of JS dataset properties extracted from the FileProperties.xml file

--- io/jsfile.py
+++ io/jsfile.py
@@ -1,5 +1,8 @@
 import os
 import dircache
+from lxml import etree
+
+from properties import FileProperties, TraceProperties
 
 # constant filenames
 JS_FILE_PROPERTIES_XML = "FileProperties.xml"
@@ -12,30 +15,39 @@ JS_HISTORY_XML = "History.xml"
 JS_TRACE_MAP = "TraceMap"
 JS_HAS_TRACES_FILE = "Status.properties"
 JS_VIRTUAL_FOLDERS_XML = "VirtualFolders.xml"
-JS_TRACE_HEADERS_XML "TraceHeaders.xml"
+JS_TRACE_HEADERS_XML = "TraceHeaders.xml"
 
 # other constants
 JS_EARLYVERSION1 = "2006.01"
 JS_PREXMLVERSION = "2006.2"
 JS_VERSION = "2006.3"
 
-def open_javaseis(filename):
-    if not os.path.isdir(filename):
+def open_javaseis(path):
+    if not os.path.isdir(path):
         raise IOError("JavaSeis dataset does not exists")
 
-    if not os.access(filename, os.R_OK):
+    if not os.access(path, os.R_OK):
         raise IOError("Missing read access for JavaSeis dataset")
 
-    js_dataset = new JavaSeisDataset(filename)
+    js_dataset = JavaSeisDataset(path)
+
 
 class JavaSeisDataset(object):
+    """
+    Class to host a JavaSeis dataset. This class will be used by both the
+    Reader and Writer classes.
+    """
     def __init__(self, path):
         if not os.path.isdir(path):
             raise IOError("Must be a folder: %s" % path)
 
         self._validate_js_dir(path)
+        self._files = dircache.listdir(path)
+        self.path = path
+        self._read_properties()
 
-    def _validate_js_dir(files):
+
+    def _validate_js_dir(self, path):
         def js_missing(f):
             raise IOError("Missing: "+f)
         files = dircache.listdir(path)
@@ -60,6 +72,39 @@ class JavaSeisDataset(object):
 
         return True
 
+    def _read_properties(self):
+        filename = os.path.join(self.path, JS_FILE_PROPERTIES_XML)
+
+        data = None
+        with open(filename, 'r') as f:
+            data = f.read()
+
+        root = etree.XML(data)
+        if root.get('name') != 'JavaSeis Metadata':
+            raise IOError(JS_FILE_PROPERTIES_XML+" is not a JavaSeis Metadata file!")
+
+        children_nodes = list(root)
+
+        def get_parset(name):
+            tmp = [x for x in children_nodes if x.get('name') == name]
+            if tmp and len(tmp) == 1:
+                return tmp[0]
+            else:
+                return None
+
+        parset_file_properties = get_parset('FileProperties')
+        parset_trace_properties = get_parset('TraceProperties')
+        parset_custom_properties = get_parset('CustomProperties')
+
+        self._file_properties = FileProperties(parset_file_properties)
+        print(self._file_properties)
+        print(self._file_properties._attributes)
+
+        #self._trace_properties = TraceProperties(parset_trace_properties)
+        #print(self._trace_properties)
+
+    def get_nr_dimension(self):
+        return self._file_properties
 
 
 class JSFileReader(object):
@@ -69,4 +114,12 @@ class JSFileReader(object):
         self._headerBuffer = None
         self._traceMap = None
 
-    def open(self, file_name):
+    def open(self, path):
+        js_dataset = open_javaseis(path)
+
+
+
+if __name__ == '__main__':
+    testpath = "/home/asbjorn/datasets/2hots.js"
+    jsDataset = JavaSeisDataset(testpath)
+    print jsDataset
