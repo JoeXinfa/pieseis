832d8b2d20aa7734c52c0cd9f4b50f7ebee442c0
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-05-27 13:51:14 +0200

    added a GridDefinition class, mainly a placeholder for some definitions (enum -ish style)

---
+++ pyjavaseis/io/defs.py
@@ -0,0 +1,17 @@
+
+
+class GridDefinition(object):
+    SAMPLE_INDEX = 0
+    TRACE_INDEX = 1
+    FRAME_INDEX = 2
+    VOLUME_INDEX = 3
+    HYPERCUBE_INDEX = 4
+
+
+    SHOT = 0
+    CDP = 1
+    OFFSET = 2
+    VOLUME = 3
+
+    def __init__(self):
+        pass
