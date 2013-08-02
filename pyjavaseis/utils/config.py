c7e6431dc1e8b5a37bb4bb11903fdbdf9522f99f
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-08-02 22:31:08 +0200

    using a config file

---
+++ pyjavaseis/utils/config.py
@@ -0,0 +1,12 @@
+"""
+This python config files sets the most used config constants and such
+"""
+import os
+
+NAME = "pyjavaseis"
+LOG_FILE = "pyseis.log"
+
+# While debugging / developing, set by default to True
+#DEBUG = os.getenv("DEBUG")
+DEBUG = True
+
