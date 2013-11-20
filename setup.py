ebf224b99813eb45af2ae4f96ef5e2232649a6e5
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-11-20 13:26:56 +0100

    bugfix: missing commas

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     name     = 'PyJavaSeis',
     version  = '0.1dev',
     author   = u"Asbjørn Alexander Fellinghaug",
-    author_email = "asbjorn <dot> fellinghaug _dot_ com"
+    author_email = "asbjorn <dot> fellinghaug _dot_ com",
     packages = ['pyjavaseis'],
     license  = 'GNU LGPL',
     long_description = """
 PyJavaSeis is a Python implementation of the seismic file format named JavaSeis.
-    """
+    """,
     install_requires=[
         "lxml>=3.1.0"
     ]
