e319ff3995adbcdb653b1eaba296ef1d6a8b936b
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-11-25 13:06:20 +0100

    remove not needed log stmt

--- pyjavaseis/__init__.py
+++ pyjavaseis/__init__.py
@@ -4,7 +4,5 @@ Python implementation of the JavaSeis seismic file format.
 """
 
 from pyjavaseis.utils.log import logger
-logger.info("Starting up pyjavaseis library..")
-
 
 __all__ = ['io','utils']
