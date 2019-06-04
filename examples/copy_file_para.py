383b2bb9edcec57b16d2ec6267116dec36f32f55
Xinfa Joseph Zhu <xinfazhu@gmail.com>
2019-06-03 22:25:40 -0500

    Add example to copy file using multi threads

---
+++ examples/copy_file_para.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Copy a JavaSeis dataset using multi threads
+"""
+
+# cd the example directory
+import sys
+sys.path.append('..')
+
+import pieseis.io.jsfile as jsfile
+from joblib import Parallel, delayed
+
+import os
+NCORE = os.cpu_count()
+
+PDH = '/data/my_primary/PromaxDataHome/'
+fnsrc = PDH + 'area/line/data.js'
+fndst = PDH + 'area/line/data_copy.js'
+
+jsdsrc = jsfile.JavaSeisDataset.open(fnsrc)
+jsddst = jsfile.JavaSeisDataset.open(fndst, mode='w', similar_to=fnsrc)
+
+def write1frame(jsdsrc, jsddst, iframe):
+    if iframe % 10 == 0:
+        print("Reading frame {} of {}".format(iframe, jsdsrc.nframes))
+    idx = jsdsrc.ind2sub(iframe)
+    trcs, hdrs = jsdsrc.read_frame(idx)
+    fold = jsdsrc.fold(idx)
+    if fold > 0:
+        jsddst.write_frame(trcs, hdrs, fold, iframe)
+
+Parallel(n_jobs=NCORE)(delayed(write1frame)(jsdsrc, jsddst, i+1)
+    for i in range(jsdsrc.nframes))
